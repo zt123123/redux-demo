1fcdc6ab17f7742fa876fede25381df009cc9f42
ZhangTian <249812928@qq.com>
2018-10-24 21:59:09 +0800

    多个state.set改用state.merge

--- src/header/store/reducer.js
+++ src/header/store/reducer.js
@@ -10,10 +10,18 @@ let defaultState = fromJS({
 export default (state = defaultState, action) => {
     switch (action.type) {
         case HEADER_GET_NAME:
-            return state
-                .set("name", action.data.name)
-                .set("age", action.data.age)
-                .set("love", action.data.love)
+            //bad
+            // return state
+            //     .set("name", action.data.name)
+            //     .set("age", action.data.age)
+            //     .set("love", action.data.love)
+            
+            //good
+            return state.merge({
+                "name": ction.data.name,
+                "age": action.data.age,
+                "love": action.data.love
+            })
         default:
             break;
     }
